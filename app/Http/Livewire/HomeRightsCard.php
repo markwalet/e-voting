acdb16bbaa92987ab17bf0c9f8656ed7e78ad096
Roelof Roos <github@roelof.io>
2020-10-30 21:07:30 +0100

    Missing '$user' fix

--- app/Http/Livewire/HomeRightsCard.php
+++ app/Http/Livewire/HomeRightsCard.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class HomeRightsCard extends Component
 {
     public function render()
     {
-        return view('livewire.home-rights-card');
+        return view('livewire.home-rights-card', [
+            'user' => Auth::user()
+        ]);
     }
 }
