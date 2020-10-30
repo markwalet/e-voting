f2e8c9e7ba34cd46bb57d367a2da020726f1aff5
Roelof Roos <github@roelof.io>
2020-10-31 00:40:14 +0100

    Raise polling interval from 5s to 60s

--- resources/views/livewire/poll-vote-list.blade.php
+++ resources/views/livewire/poll-vote-list.blade.php
@@ -1,6 +1,6 @@
-<div wire:poll.5s>
+<div wire:poll.60s>
     {{-- Message that this updates --}}
-    <div class="text-right text-sm text-gray-500 mb-4">De lijst updatet iedere 5 seconden</div>
+    <div class="text-right text-sm text-gray-500 mb-4">De lijst updatet automatisch</div>
     @forelse ($this->polls as $index => $poll)
     <livewire:poll-vote-card :poll="$poll" :key="'poll-'.$poll->id" />
     @empty
